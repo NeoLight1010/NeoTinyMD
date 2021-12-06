bbf8e8215c39115db2fc7df0bdde245c1f3809b7
Anthony Suárez <neolight1010@gmail.com>
2021-12-06 11:50:49 -0500

    Add banner functions.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+fn parse_markdown_file() {
+
+}
+
+fn get_title() -> String {
+    let mut title = String::from(env!("CARGO_PKG_NAME"));
+    title.push_str(" (v");
+    title.push_str(env!("CARGO_PKG_VERSION"));
+    title.push_str("), ");
+    title.push_str(env!("CARGO_PKG_DESCRIPTION"));
+
+    title
+}
+
+fn print_short_banner() {
+    println!("{}", get_title());
+}
+
+fn print_long_banner() {
+    print_short_banner();
+    println!("Written by: {}\nHomepage: {}\nUsage: neotinymd <filename>.md", env!("CARGO_PKG_AUTHORS"), env!("CARGO_PKG_HOMEPAGE"));
+}
+
+fn usage() {
+    print_long_banner();
+}
+
 fn main() {
-    println!("Hello, world!");
+    usage();
 }
